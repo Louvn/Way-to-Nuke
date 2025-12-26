8a598e396a82548d8bee1b53cbecb834a2f33a2b
Louvn <m.rauschen11@gmail.com>
2025-12-26 17:05:40 +0100

    Fix typo in API name from 'evesrlyAPI' to 'everlyAPI'

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const shopItemCardTemplate = document.getElementById("shopItemCardTemplate");
 
 // Load Saved Game
 let savedGame;
-if ("evesrlyAPI" in window) {
+if ("everlyAPI" in window) {
     everlyAPI.loadGame((data) => {
         savedGame = data;
         startGame();
